8ec01209e973a4791bb0359c20d1ff13cc9662c9
gsteiner1992 <gsteiner1992@gmail.com>
2018-07-12 22:13:12 -0600

    Componente de artistas

---
+++ client/src/app/components/artist-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+
+import { GLOBAL } from "../services/global";
+import { UserService } from "../services/user.service";
+import { Artist } from "../models/artist";
+
+@Component({
+    selector: 'artist-list',
+    templateUrl: '../views/artist-list.html',
+    providers: [UserService]
+})
+
+export class ArtistListComponent implements OnInit {
+    public titulo: String;
+    public artists: Artist[];
+    public identity;
+    public token;
+    public url: String;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userService: UserService
+    ){
+        this.titulo = 'Artistas';
+        this.identity = _userService.getIdentity(),
+        this.token = _userService.getToken();
+        this.url = GLOBAL.url;
+    }
+
+    ngOnInit(): void {
+        console.log('Artist list component cargado');
+
+        //Conseguir listado 
+    }
+}
